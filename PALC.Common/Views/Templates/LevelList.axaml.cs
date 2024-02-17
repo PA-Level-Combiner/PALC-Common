8e6ad00bb551114adb149d8eb560ae5430c94119
TNTzx <azelynnrituapersonality@gmail.com>
2024-02-17 23:40:58 +0800

    wawa

--- PALC.Common/Views/Templates/LevelList.axaml.cs
+++ PALC.Common/Views/Templates/LevelList.axaml.cs
@@ -1,11 +1,12 @@
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using System.Windows.Input;
 
 namespace PALC.Common.Views.Templates;
 
 
-public partial class LevelList : ContentControl
+public partial class LevelList : TemplatedControl
 {
     public static readonly StyledProperty<object> ListBoxProperty =
         AvaloniaProperty.Register<LevelList, object>(nameof(ListBox));
@@ -16,12 +17,31 @@ public partial class LevelList : ContentControl
     }
 
 
+
+    public static readonly StyledProperty<bool> AddItemEnabledProperty =
+        AvaloniaProperty.Register<LevelList, bool>(nameof(AddItemEnabled), true);
+    public bool AddItemEnabled
+    {
+        get => GetValue(AddItemEnabledProperty);
+        set => SetValue(AddItemEnabledProperty, value);
+    }
+
+
     public static readonly StyledProperty<ICommand> AddItemCommandProperty =
         AvaloniaProperty.Register<LevelList, ICommand>(nameof(AddItemCommand));
     public ICommand AddItemCommand
     {
         get => GetValue(AddItemCommandProperty);
         set => SetValue(AddItemCommandProperty, value);
+    }
+
+
+    public static readonly StyledProperty<bool> DeleteItemEnabledProperty =
+        AvaloniaProperty.Register<LevelList, bool>(nameof(DeleteItemEnabled), true);
+    public bool DeleteItemEnabled
+    {
+        get => GetValue(DeleteItemEnabledProperty);
+        set => SetValue(DeleteItemEnabledProperty, value);
     }
 
 
